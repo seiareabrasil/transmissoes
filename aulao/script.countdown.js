70791ef8fe1520958a354a0e808db31f414b9a8a
seiareabrasil <85242939+seiareabrasil@users.noreply.github.com>
2021-06-02 23:39:16 -0300

    Update script.countdown.js

--- aulao/script.countdown.js
+++ aulao/script.countdown.js
@@ -1,7 +1,7 @@
 $(function(){
 	
 	var note = $('#note'),
-		ts = new Date('2021/06/10 18:00:00'),
+		ts = new Date('2021/06/10 19:00:00'),
 		// ts = new Date('2021/02/27 13:40:00'),
 		newYear = true;
 	
